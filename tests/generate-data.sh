dd7a02b0e7e437c256a405c6b17639e90e2017d3
Haran Shivanan <shivanan@statictype.org>
2021-06-23 20:41:33 +0530

    updated energy model and test data

--- tests/generate-data.sh
+++ tests/generate-data.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 APIKEY="$2"
 URL="$1"
-curl -X POST $URL/Lucy/EnergyBudget/budget/building1/mechanical  -H "Authorization: APIKEY $APIKEY" -H "Content-Type: application/json" --data '{ "budget": [100,150,200,150,150,150,200,200,200,200,200,200] }'
-curl -X POST $URL/Lucy/EnergyBudget/budget/building1/electrical  -H "Authorization: APIKEY $APIKEY" -H "Content-Type: application/json" --data '{ "budget": [50,50,50,50,50,50,50,50,50,50,50,50] }'
+curl -X POST $URL/Lucy/EnergyBudget/budget/building1/electrical  -H "Authorization: APIKEY $APIKEY" -H "Content-Type: application/json" --data '{ "budget": [100,150,200,150,150,150,200,200,200,200,200,200] }'
+curl -X POST $URL/Lucy/EnergyBudget/budget/building1/mechanical  -H "Authorization: APIKEY $APIKEY" -H "Content-Type: application/json" --data '{ "budget": [50,50,50,50,50,50,50,50,50,50,50,50] }'
 
 
 curl -X POST $URL/Lucy/EnergyBudget/categories  -H "Authorization: APIKEY $APIKEY" -H "Content-Type: application/json" --data '{ "id":"mechanical","label":"Mechanical"}'
@@ -25,7 +25,8 @@ curl -X POST $URL/Lucy/EnergyBudget/consumption/building1/electrical/2021/1 -H "
 curl -X POST $URL/Lucy/EnergyBudget/consumption/building1/electrical/2021/2 -H "Authorization: APIKEY $APIKEY" -H "Content-Type: application/json" --data '{ "value": 230 }'
 curl -X POST $URL/Lucy/EnergyBudget/consumption/building1/electrical/2021/3 -H "Authorization: APIKEY $APIKEY" -H "Content-Type: application/json" --data '{ "value": 200 }'
 curl -X POST $URL/Lucy/EnergyBudget/consumption/building1/electrical/2021/4 -H "Authorization: APIKEY $APIKEY" -H "Content-Type: application/json" --data '{ "value": 210 }'
-curl -X POST $URL/Lucy/EnergyBudget/consumption/building1/electrical/2021/5 -H "Authorization: APIKEY $APIKEY" -H "Content-Type: application/json" --data '{ "value": 20 }'
+curl -X POST $URL/Lucy/EnergyBudget/consumption/building1/electrical/2021/5 -H "Authorization: APIKEY $APIKEY" -H "Content-Type: application/json" --data '{ "value": 200 }'
+curl -X POST $URL/Lucy/EnergyBudget/consumption/building1/electrical/2021/6 -H "Authorization: APIKEY $APIKEY" -H "Content-Type: application/json" --data '{ "value": 20 }'
 
 
 curl -X POST $URL/Lucy/EnergyBudget/consumption/building1/mechanical/2020/1  -H "Authorization: APIKEY $APIKEY" -H "Content-Type: application/json" --data '{ "value": 30}'
@@ -44,4 +45,5 @@ curl -X POST $URL/Lucy/EnergyBudget/consumption/building1/mechanical/2021/1 -H "
 curl -X POST $URL/Lucy/EnergyBudget/consumption/building1/mechanical/2021/2 -H "Authorization: APIKEY $APIKEY" -H "Content-Type: application/json" --data '{ "value": 30}'
 curl -X POST $URL/Lucy/EnergyBudget/consumption/building1/mechanical/2021/3 -H "Authorization: APIKEY $APIKEY" -H "Content-Type: application/json" --data '{ "value": 30}'
 curl -X POST $URL/Lucy/EnergyBudget/consumption/building1/mechanical/2021/4 -H "Authorization: APIKEY $APIKEY" -H "Content-Type: application/json" --data '{ "value": 30}'
-curl -X POST $URL/Lucy/EnergyBudget/consumption/building1/mechanical/2021/5 -H "Authorization: APIKEY $APIKEY" -H "Content-Type: application/json" --data '{ "value": 5}'
+curl -X POST $URL/Lucy/EnergyBudget/consumption/building1/mechanical/2021/5 -H "Authorization: APIKEY $APIKEY" -H "Content-Type: application/json" --data '{ "value": 30}'
+curl -X POST $URL/Lucy/EnergyBudget/consumption/building1/mechanical/2021/6 -H "Authorization: APIKEY $APIKEY" -H "Content-Type: application/json" --data '{ "value": 5}'
