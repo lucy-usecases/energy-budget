2de6dad1cb69cd8cc9afb8bb8c20f8dff3c8b194
Haran Shivanan <shivanan@statictype.org>
2021-07-01 22:56:59 +0530

    added carbon offset support

--- dist/main.js
+++ dist/main.js
@@ -312,7 +312,7 @@ module.exports = function () {
 var ___CSS_LOADER_API_IMPORT___ = __webpack_require__(/*! ../node_modules/css-loader/dist/runtime/api.js */ "./node_modules/css-loader/dist/runtime/api.js");
 exports = ___CSS_LOADER_API_IMPORT___(false);
 // Module
-exports.push([module.i, ".selector-energy {\n  margin-bottom: 10px; }\n", ""]);
+exports.push([module.i, ".selector-energy {\n  margin-bottom: 10px; }\n\n.energy-gauge .gauge-face-container {\n  margin-left: auto;\n  margin-right: auto; }\n\n.carbon-footprint {\n  padding: 10px 50px; }\n  .carbon-footprint .profile-image-container {\n    background-color: transparent !important; }\n  .carbon-footprint .cf-grid .data-grid-column {\n    border: none !important; }\n  .carbon-footprint .cf-grid .item-card {\n    padding: 5px; }\n    .carbon-footprint .cf-grid .item-card .title {\n      font-size: 2.4em;\n      font-weight: 200; }\n    .carbon-footprint .cf-grid .item-card .content {\n      padding: 0px; }\n", ""]);
 // Exports
 module.exports = exports;
 
@@ -10477,6 +10477,9 @@ const uxp_1 = __webpack_require__(/*! ./uxp */ "./src/uxp.ts");
 const components_1 = __webpack_require__(/*! uxp/components */ "uxp/components");
 __webpack_require__(/*! ./styles.scss */ "./src/styles.scss");
 const EnergyIcon = 'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIzMC44NDgiIGhlaWdodD0iMzAuODQ5IiB2aWV3Qm94PSIwIDAgMzAuODQ4IDMwLjg0OSI+CiAgPHBhdGggaWQ9Ikljb25fbWV0cm8tcG93ZXIiIGRhdGEtbmFtZT0iSWNvbiBtZXRyby1wb3dlciIgZD0iTTE0LjEzOSwxLjkyOCwyLjU3MSwxNy4zNTJIMTQuMTM5TDYuNDI3LDMyLjc3NywzMy40MTksMTMuNUgxNy45OTVMMjkuNTYzLDEuOTI4WiIgdHJhbnNmb3JtPSJ0cmFuc2xhdGUoLTIuNTcxIC0xLjkyOCkiLz4KPC9zdmc+Cg==';
+const PowerIcon = 'data:image/svg+xml,%3Csvg xmlns="http://www.w3.org/2000/svg" width="33" height="33" viewBox="0 0 33 33"%3E%3Cdefs%3E%3Cstyle%3E.a%7Bfill:%23535353;%7D.b%7Bfill:%23fff;%7D%3C/style%3E%3C/defs%3E%3Ccircle class="a" cx="16.5" cy="16.5" r="16.5"/%3E%3Cpath class="b" d="M7.316,1.928,2.571,8.256H7.316L4.153,14.583,15.226,6.674H8.9l4.746-4.746Z" transform="translate(7.385 8.602)"/%3E%3C/svg%3E';
+const Co2Icon = 'data:image/svg+xml,%3Csvg xmlns="http://www.w3.org/2000/svg" width="33" height="33" viewBox="0 0 33 33"%3E%3Cdefs%3E%3Cstyle%3E.a%7Bfill:%23535353;%7D.b%7Bfill:%23fff;%7D%3C/style%3E%3C/defs%3E%3Ccircle class="a" cx="16.5" cy="16.5" r="16.5"/%3E%3Cpath class="b" d="M7.608,12.061a3.251,3.251,0,0,1,.734-2.08A3.188,3.188,0,0,1,10.2,8.817a4.046,4.046,0,0,1,6.454-2.311,3.955,3.955,0,0,1,1.428,2.22H18.3A3.258,3.258,0,0,1,20.68,9.7a3.2,3.2,0,0,1,.982,2.352,3.253,3.253,0,0,1-.305,1.4,3.377,3.377,0,0,1-.85,1.131v.025a2.07,2.07,0,0,1-.462,1.312,2.007,2.007,0,0,1-1.155.734,2.574,2.574,0,0,1-2.1,1.964,1.443,1.443,0,0,1-1.081,2.385,1.36,1.36,0,0,1-1.015-.429,1.4,1.4,0,0,1-.421-1.023,1.347,1.347,0,0,1,.083-.47h-.083a1.775,1.775,0,0,1-1.758-1.758,1.6,1.6,0,0,1,.248-.883,2.05,2.05,0,0,1-.924-1.032H10.785V15.4a3.323,3.323,0,0,1-2.253-1.048A3.184,3.184,0,0,1,7.608,12.061Zm1.155-.206a2.367,2.367,0,0,0,2.368,2.377,2.32,2.32,0,0,0,1.131-.281,2.529,2.529,0,0,0,.858,1.535,2.448,2.448,0,0,0,1.667.611,2.5,2.5,0,0,0,1.832-.759,1.591,1.591,0,0,0,1.271.586,1.691,1.691,0,0,0,1.7-1.7,2.374,2.374,0,0,0,.784-.858,2.327,2.327,0,0,0,.289-1.131,2.232,2.232,0,0,0-.7-1.667,2.349,2.349,0,0,0-1.692-.685,2.323,2.323,0,0,0-1.263.363,2.729,2.729,0,0,0,.066-.652,2.743,2.743,0,0,0-.85-2.03,2.931,2.931,0,0,0-4.06-.033,2.724,2.724,0,0,0-.883,1.956h-.14a2.3,2.3,0,0,0-1.675.693A2.276,2.276,0,0,0,8.763,11.854Z" transform="translate(1.332 3.3)"/%3E%3C/svg%3E';
+const TreesIcon = 'data:image/svg+xml,%3Csvg xmlns="http://www.w3.org/2000/svg" width="33" height="33" viewBox="0 0 33 33"%3E%3Cdefs%3E%3Cstyle%3E.a%7Bfill:%23535353;%7D.b%7Bfill:%23fff;%7D%3C/style%3E%3C/defs%3E%3Ccircle class="a" cx="16.5" cy="16.5" r="16.5"/%3E%3Cpath class="b" d="M14.215,4.221a.43.43,0,0,1-.2-.369,4.239,4.239,0,0,0-8.443,0,.432.432,0,0,1-.2.369A4.25,4.25,0,0,0,6.717,11.8a1.159,1.159,0,0,0,.738.039,1.953,1.953,0,0,0,.369-1.433,1.4,1.4,0,0,0-.657-.328A2.506,2.506,0,0,1,5.324,7.664,2.466,2.466,0,0,1,5.98,5.981a2.3,2.3,0,0,1,.614-.49,1.451,1.451,0,0,0,.738-1.272,2.357,2.357,0,0,1,.122-.778,2.5,2.5,0,0,1,4.753,0,2.357,2.357,0,0,1,.122.778,1.365,1.365,0,0,0,.739,1.264,1.722,1.722,0,0,1,.612.487,2.445,2.445,0,0,1,.657,1.682,2.485,2.485,0,0,1-2.5,2.5,2.87,2.87,0,0,0-2.869,2.859v1.633a1.528,1.528,0,0,0,.123.739,1.849,1.849,0,0,0,1.475,0,1.421,1.421,0,0,0,.126-.739V12.973A1.109,1.109,0,0,1,11.8,11.864,4.211,4.211,0,0,0,14.225,4.2l-.01.021Z" transform="translate(7 8.769)"/%3E%3C/svg%3E';
 let startYear = new Date().getFullYear();
 let Years = [];
 for (var i = 0; i < 3; i++) {
@@ -10550,6 +10553,7 @@ const EnergyBudgetWidget = (props) => {
     let [chartData, setChartData] = React.useState([]);
     let [categories, setCategories] = React.useState([]);
     let [selectedCategory, setSelectedCategory] = React.useState('');
+    let [co2, setCo2] = React.useState(0);
     let updater = components_1.useUpdateWidgetProps();
     function loadLocations() {
         return __awaiter(this, void 0, void 0, function* () {
@@ -10597,12 +10601,15 @@ const EnergyBudgetWidget = (props) => {
         loadLocations().then(_ => { });
     }, []);
     React.useEffect(() => {
-        props.uxpContext.executeAction(model, 'ConsumptionForLocation', { year, location: selectedBuilding, category: selectedCategory }, { json: true }).then((data) => {
+        props.uxpContext.executeAction(model, 'ConsumptionForLocationWithEmission', { year, location: selectedBuilding, category: selectedCategory }, { json: true }).then((data) => {
             let consumptionData = {};
+            let _co2 = 0;
             for (var j in data) {
                 let row = data[j];
                 consumptionData[Number(row.month)] = Number(row.value);
+                _co2 = Number(row.co2) || 0;
             }
+            setCo2(_co2);
             chartData = [];
             let cummulativeBudget = 0;
             let cummulativeEnergy = 0;
@@ -10621,13 +10628,30 @@ const EnergyBudgetWidget = (props) => {
             updater(props.instanceId, { year, building: selectedBuilding, category: selectedCategory });
         });
     }, [year, selectedBuilding, selectedCategory]);
+    let totalConsumption = 0;
+    if (chartData && chartData.length > 0) {
+        totalConsumption = chartData[chartData.length - 1].cummulativeEnergy;
+    }
+    let showCO2 = false;
+    let emissions = totalConsumption * co2 / (1000 * 1000);
+    let trees = 16.5 * emissions;
+    if (co2 > 0)
+        showCO2 = true;
     return (React.createElement(components_1.WidgetWrapper, null,
         React.createElement(components_1.TitleBar, { icon: EnergyIcon, title: 'Yearly Energy Budgeted vs Actual ' + (selectedBuilding ? `${selectedBuilding} - ${year}` : '') + ' ' + (selectedCategory ? `[${selectedCategory}]` : '') },
             React.createElement(components_1.FilterPanel, { enableClear: false },
                 React.createElement(components_1.Select, { className: 'selector-energy', placeholder: 'Year', onChange: (year) => setYear(year), options: Years, selected: year }),
                 React.createElement(components_1.Select, { className: 'selector-energy', placeholder: 'Location', onChange: (b) => selectBuilding(b), selected: selectedBuilding, options: buildings, labelField: 'location', valueField: 'location' }),
                 React.createElement(components_1.Select, { className: 'selector-energy', placeholder: 'Energy Type', onChange: (b) => selectCategory(b), selected: selectedCategory, options: categories, labelField: 'label', valueField: 'id' }))),
-        React.createElement("div", { style: { flex: 1, padding: '30px', paddingBottom: '45px' } },
+        showCO2 && React.createElement("div", { className: 'carbon-footprint' },
+            React.createElement(components_1.DataGrid, { className: 'cf-grid', data: [
+                    { 'title': 'power consumed', value: `${totalConsumption} kWh`, image: PowerIcon },
+                    { 'title': 'co2 emitted', value: `${(emissions).toFixed(1)} tonnes`, image: Co2Icon },
+                    { 'title': 'trees to offset co2', value: `${(trees).toFixed(0)} `, image: TreesIcon },
+                ], columns: 3, renderItem: (item) => {
+                    return React.createElement(components_1.ItemCard, { imageField: 'image', item: item, titleField: 'value', subTitleField: 'title' });
+                } })),
+        React.createElement("div", { style: { flex: 1, padding: '30px', marginBottom: '45px' } },
             React.createElement(recharts_1.ResponsiveContainer, { width: "100%", height: "100%" },
                 React.createElement(recharts_1.ComposedChart, { width: 500, height: 300, data: chartData, margin: {
                         top: 5,
@@ -10644,10 +10668,10 @@ const EnergyBudgetWidget = (props) => {
                     React.createElement(recharts_1.YAxis, { axisLine: false, orientation: 'right', yAxisId: 'cummulative' }),
                     React.createElement(recharts_1.Tooltip, null),
                     React.createElement(recharts_1.Legend, null),
+                    React.createElement(recharts_1.Area, { filter: "url(#shadow)", name: 'Cummulative Consumption', yAxisId: 'cummulative', fill: '#06F', fillOpacity: 0.5, stroke: '#06F', dataKey: "cummulativeEnergy" }),
                     React.createElement(recharts_1.Bar, { name: 'Consumption', barSize: 15, dataKey: "energy", fill: '#F78FAA' }),
                     React.createElement(recharts_1.Bar, { name: 'Baseline', barSize: 15, dataKey: "budgeted", fill: "#79B7B6" }),
-                    React.createElement(recharts_1.Line, { name: 'Cummulative Budget', strokeDasharray: '0 1 1 1', strokeDashoffset: 3, strokeOpacity: 0.8, strokeWidth: 2, yAxisId: 'cummulative', type: "monotone", fill: 'red', fillOpacity: 0.1, dataKey: "cummulativeBudget", stroke: "#ff7300" }),
-                    React.createElement(recharts_1.Area, { filter: "url(#shadow)", name: 'Cummulative Consumption', yAxisId: 'cummulative', fill: '#06F', fillOpacity: 0.5, stroke: '#06F', dataKey: "cummulativeEnergy" }))))));
+                    React.createElement(recharts_1.Line, { name: 'Cummulative Budget', strokeDasharray: '0 1 1 1', strokeDashoffset: 3, strokeOpacity: 0.8, strokeWidth: 2, yAxisId: 'cummulative', type: "monotone", fill: 'red', fillOpacity: 0.1, dataKey: "cummulativeBudget", stroke: "#ff7300" }))))));
 };
 exports.EnergyBreakdown = (props) => {
     let [year, setYear] = React.useState('');
@@ -10792,7 +10816,7 @@ exports.CurrentUsage = (props) => {
         updater(props.instanceId, { category: selectedCategory, building });
     }), [building, budget, selectedCategory]);
     let budgetValue = Number(budget);
-    return React.createElement(components_1.WidgetWrapper, null,
+    return React.createElement(components_1.WidgetWrapper, { className: 'energy-gauge' },
         React.createElement(components_1.TitleBar, { title: 'Current Monthly Energy Usage' },
             React.createElement(components_1.FilterPanel, { enableClear: false },
                 React.createElement(components_1.Select, { className: 'selector-energy', onChange: selectBuilding, selected: building, options: buildings, labelField: 'location', valueField: 'location' }),
--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,17 @@
 import * as React from "react";
 import { Bar, BarChart, CartesianGrid, Legend, ResponsiveContainer, XAxis, YAxis, Tooltip, ComposedChart, Line, Area, PieChart, Pie, Cell } from 'recharts';
 import { registerWidget, registerLink, registerUI, IContextProvider, } from './uxp';
-import { TitleBar, FilterPanel, WidgetWrapper, Select, useUpdateWidgetProps, RadialGauge, Checkbox, useMessageBus } from "uxp/components";
+import { TitleBar, FilterPanel, WidgetWrapper, Select, useUpdateWidgetProps, RadialGauge, Checkbox, useMessageBus, DataGrid, ItemCard } from "uxp/components";
 import './styles.scss';
 import { getPositionOfLineAndCharacter } from "typescript";
 
 
 
 const EnergyIcon = 'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIzMC44NDgiIGhlaWdodD0iMzAuODQ5IiB2aWV3Qm94PSIwIDAgMzAuODQ4IDMwLjg0OSI+CiAgPHBhdGggaWQ9Ikljb25fbWV0cm8tcG93ZXIiIGRhdGEtbmFtZT0iSWNvbiBtZXRyby1wb3dlciIgZD0iTTE0LjEzOSwxLjkyOCwyLjU3MSwxNy4zNTJIMTQuMTM5TDYuNDI3LDMyLjc3NywzMy40MTksMTMuNUgxNy45OTVMMjkuNTYzLDEuOTI4WiIgdHJhbnNmb3JtPSJ0cmFuc2xhdGUoLTIuNTcxIC0xLjkyOCkiLz4KPC9zdmc+Cg==';
+const PowerIcon = 'data:image/svg+xml,%3Csvg xmlns="http://www.w3.org/2000/svg" width="33" height="33" viewBox="0 0 33 33"%3E%3Cdefs%3E%3Cstyle%3E.a%7Bfill:%23535353;%7D.b%7Bfill:%23fff;%7D%3C/style%3E%3C/defs%3E%3Ccircle class="a" cx="16.5" cy="16.5" r="16.5"/%3E%3Cpath class="b" d="M7.316,1.928,2.571,8.256H7.316L4.153,14.583,15.226,6.674H8.9l4.746-4.746Z" transform="translate(7.385 8.602)"/%3E%3C/svg%3E';
+const Co2Icon = 'data:image/svg+xml,%3Csvg xmlns="http://www.w3.org/2000/svg" width="33" height="33" viewBox="0 0 33 33"%3E%3Cdefs%3E%3Cstyle%3E.a%7Bfill:%23535353;%7D.b%7Bfill:%23fff;%7D%3C/style%3E%3C/defs%3E%3Ccircle class="a" cx="16.5" cy="16.5" r="16.5"/%3E%3Cpath class="b" d="M7.608,12.061a3.251,3.251,0,0,1,.734-2.08A3.188,3.188,0,0,1,10.2,8.817a4.046,4.046,0,0,1,6.454-2.311,3.955,3.955,0,0,1,1.428,2.22H18.3A3.258,3.258,0,0,1,20.68,9.7a3.2,3.2,0,0,1,.982,2.352,3.253,3.253,0,0,1-.305,1.4,3.377,3.377,0,0,1-.85,1.131v.025a2.07,2.07,0,0,1-.462,1.312,2.007,2.007,0,0,1-1.155.734,2.574,2.574,0,0,1-2.1,1.964,1.443,1.443,0,0,1-1.081,2.385,1.36,1.36,0,0,1-1.015-.429,1.4,1.4,0,0,1-.421-1.023,1.347,1.347,0,0,1,.083-.47h-.083a1.775,1.775,0,0,1-1.758-1.758,1.6,1.6,0,0,1,.248-.883,2.05,2.05,0,0,1-.924-1.032H10.785V15.4a3.323,3.323,0,0,1-2.253-1.048A3.184,3.184,0,0,1,7.608,12.061Zm1.155-.206a2.367,2.367,0,0,0,2.368,2.377,2.32,2.32,0,0,0,1.131-.281,2.529,2.529,0,0,0,.858,1.535,2.448,2.448,0,0,0,1.667.611,2.5,2.5,0,0,0,1.832-.759,1.591,1.591,0,0,0,1.271.586,1.691,1.691,0,0,0,1.7-1.7,2.374,2.374,0,0,0,.784-.858,2.327,2.327,0,0,0,.289-1.131,2.232,2.232,0,0,0-.7-1.667,2.349,2.349,0,0,0-1.692-.685,2.323,2.323,0,0,0-1.263.363,2.729,2.729,0,0,0,.066-.652,2.743,2.743,0,0,0-.85-2.03,2.931,2.931,0,0,0-4.06-.033,2.724,2.724,0,0,0-.883,1.956h-.14a2.3,2.3,0,0,0-1.675.693A2.276,2.276,0,0,0,8.763,11.854Z" transform="translate(1.332 3.3)"/%3E%3C/svg%3E';
+const TreesIcon = 'data:image/svg+xml,%3Csvg xmlns="http://www.w3.org/2000/svg" width="33" height="33" viewBox="0 0 33 33"%3E%3Cdefs%3E%3Cstyle%3E.a%7Bfill:%23535353;%7D.b%7Bfill:%23fff;%7D%3C/style%3E%3C/defs%3E%3Ccircle class="a" cx="16.5" cy="16.5" r="16.5"/%3E%3Cpath class="b" d="M14.215,4.221a.43.43,0,0,1-.2-.369,4.239,4.239,0,0,0-8.443,0,.432.432,0,0,1-.2.369A4.25,4.25,0,0,0,6.717,11.8a1.159,1.159,0,0,0,.738.039,1.953,1.953,0,0,0,.369-1.433,1.4,1.4,0,0,0-.657-.328A2.506,2.506,0,0,1,5.324,7.664,2.466,2.466,0,0,1,5.98,5.981a2.3,2.3,0,0,1,.614-.49,1.451,1.451,0,0,0,.738-1.272,2.357,2.357,0,0,1,.122-.778,2.5,2.5,0,0,1,4.753,0,2.357,2.357,0,0,1,.122.778,1.365,1.365,0,0,0,.739,1.264,1.722,1.722,0,0,1,.612.487,2.445,2.445,0,0,1,.657,1.682,2.485,2.485,0,0,1-2.5,2.5,2.87,2.87,0,0,0-2.869,2.859v1.633a1.528,1.528,0,0,0,.123.739,1.849,1.849,0,0,0,1.475,0,1.421,1.421,0,0,0,.126-.739V12.973A1.109,1.109,0,0,1,11.8,11.864,4.211,4.211,0,0,0,14.225,4.2l-.01.021Z" transform="translate(7 8.769)"/%3E%3C/svg%3E';
+
 interface IWidgetProps {
   uxpContext?: IContextProvider;
   building: string;
@@ -108,6 +112,7 @@ const EnergyBudgetWidget: React.FunctionComponent<IWidgetProps> = (props) => {
   let [chartData, setChartData] = React.useState<any[]>([]);
   let [categories,setCategories] = React.useState<any[]>([]);
   let [selectedCategory,setSelectedCategory] = React.useState('');
+  let [co2,setCo2] = React.useState(0);
 
   let updater = useUpdateWidgetProps();
  
@@ -163,12 +168,16 @@ const EnergyBudgetWidget: React.FunctionComponent<IWidgetProps> = (props) => {
 
 
   React.useEffect(() => {
-    props.uxpContext.executeAction(model, 'ConsumptionForLocation', { year, location: selectedBuilding,category:selectedCategory }, { json: true }).then((data: any[]) => {
+    props.uxpContext.executeAction(model, 'ConsumptionForLocationWithEmission', { year, location: selectedBuilding,category:selectedCategory }, { json: true }).then((data: any[]) => {
       let consumptionData: any = {};
+      let _co2 = 0;
       for (var j in data) {
         let row = data[j];
         consumptionData[Number(row.month)] = Number(row.value);
+        _co2 = Number(row.co2) || 0;
       }
+      setCo2(_co2);
+
       chartData = [];
       let cummulativeBudget = 0;
       let cummulativeEnergy = 0;
@@ -189,7 +198,14 @@ const EnergyBudgetWidget: React.FunctionComponent<IWidgetProps> = (props) => {
     });
   }, [year, selectedBuilding,selectedCategory]);
 
-
+  let totalConsumption = 0;
+  if (chartData && chartData.length>0) {
+    totalConsumption = chartData[chartData.length - 1].cummulativeEnergy;
+  }
+  let showCO2 = false;
+  let emissions = totalConsumption*co2/(1000*1000);
+  let trees = 16.5*emissions;
+  if (co2 > 0) showCO2 = true;
 
   return (
     <WidgetWrapper>
@@ -205,7 +221,22 @@ const EnergyBudgetWidget: React.FunctionComponent<IWidgetProps> = (props) => {
             options={categories} labelField={'label'} valueField={'id'} />
         </FilterPanel>
       </TitleBar>
-      <div style={{ flex: 1,padding:'30px' ,paddingBottom:'45px'}}>
+      {
+        showCO2 && <div className={'carbon-footprint'}>
+          <DataGrid className='cf-grid' data={[
+            {'title':'power consumed',value:`${totalConsumption} kWh`,image:PowerIcon},
+            {'title':'co2 emitted',value:`${(emissions).toFixed(1)} tonnes`,image:Co2Icon},
+            {'title':'trees to offset co2',value:`${(trees).toFixed(0)} `,image:TreesIcon},
+          
+          ]}
+          columns={3}
+          renderItem={(item)=>{
+            return <ItemCard imageField={'image'} item={item} titleField={'value'} subTitleField={'title'} />
+          }}
+           />
+      </div>
+      }
+      <div style={{ flex: 1,padding:'30px' ,marginBottom:'45px'}}>
         <ResponsiveContainer width="100%" height="100%">
           <ComposedChart
             width={500}
@@ -230,13 +261,13 @@ const EnergyBudgetWidget: React.FunctionComponent<IWidgetProps> = (props) => {
         
             <Tooltip />
             <Legend />
-            <Bar name={'Consumption'} barSize={15} dataKey="energy" fill='#F78FAA'  />
-            <Bar name={'Baseline'}    barSize={15} dataKey="budgeted" fill="#79B7B6"/>
-            <Line name={'Cummulative Budget'} strokeDasharray={'0 1 1 1'} strokeDashoffset={3} strokeOpacity={0.8} strokeWidth={2} yAxisId={'cummulative'} type="monotone" fill={'red'} fillOpacity={0.1} dataKey="cummulativeBudget" stroke="#ff7300" />
             <Area filter="url(#shadow)"  name={'Cummulative Consumption'} yAxisId={'cummulative'} 
              fill={'#06F'} fillOpacity={0.5} 
             stroke={'#06F'}
             dataKey="cummulativeEnergy" /> 
+            <Bar name={'Consumption'} barSize={15} dataKey="energy" fill='#F78FAA'  />
+            <Bar name={'Baseline'}    barSize={15} dataKey="budgeted" fill="#79B7B6"/>
+            <Line name={'Cummulative Budget'} strokeDasharray={'0 1 1 1'} strokeDashoffset={3} strokeOpacity={0.8} strokeWidth={2} yAxisId={'cummulative'} type="monotone" fill={'red'} fillOpacity={0.1} dataKey="cummulativeBudget" stroke="#ff7300" />
 
           </ComposedChart>
         </ResponsiveContainer>
@@ -448,7 +479,7 @@ export const CurrentUsage: React.FunctionComponent<IWidgetProps> = (props) => {
    
   },[building,budget,selectedCategory]);
   let budgetValue = Number(budget);
-  return <WidgetWrapper>
+  return <WidgetWrapper className='energy-gauge'>
     <TitleBar title={'Current Monthly Energy Usage'} >
       <FilterPanel enableClear={false}>
         <Select className={'selector-energy'}  onChange={selectBuilding} selected={building}
